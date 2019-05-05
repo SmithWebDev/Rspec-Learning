0ee2d7033ef93b10d3b86d3925cfca34fa5d3bdb
Smith <smithwebdd@gmail.com>
2019-05-05 13:41:30 -0400

    Rspec Let Method

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,4 +1,6 @@
 =begin
+
+#EX. 1
 class Card              #Needs Class/Content to begin initialization
   attr_reader :type      #Defines an automatic read method... apply data argument
   def initialize(type)  #explains the argument being presented
@@ -16,6 +18,18 @@ RSpec.describe Card do                                #Always reference the clas
 end
 =end
 
+
+=begin
+  #Hook = a peice of code that runs 
+          #automatically at a specific time during the test suite
+  #Instance Variable = allows us to 
+          #persist information that would normally be lost outside 
+          #of a block/ method
+=end 
+
+
+=begin
+# EX. 2 (passing)
 class Card
   attr_reader :rank, :suit
   def initialize(rank, suit)
@@ -27,7 +41,7 @@ end
 
 RSpec.describe Card do
   it 'has a rank' do
-    card = Card.new('Ace', 'Spades')  #seperate arguments into their own indivisual test
+    card = Card.new('Ace', 'Spades')            #seperate arguments into their own indivisual test
     expect(card.rank).to eq('Ace')    
   end
   it 'has a suit' do
@@ -35,3 +49,114 @@ RSpec.describe Card do
     expect(card.suit).to eq('Spades')    
   end
 end
+=end
+
+#Regular variables do not persist between the test steps, 
+#Instance variable is able to cross the test suite lines.
+#DRY it up
+
+=begin
+# EX. 2 (refactored)
+class Card
+  attr_reader :rank, :suit
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+end
+
+RSpec.describe Card do
+  before do                                                 #Ability to run code prior to the test suit (HOOK)
+    @card = Card.new('Ace', 'Spades')                        #seperate arguments into their own indivisual test
+  end
+  it 'has a rank' do
+    @card = Card.new('Ace', 'Spades')                        #seperate arguments into their own indivisual test
+    expect(@card.rank).to eq('Ace')    
+  end
+  it 'has a suit' do
+    @card = Card.new('Ace', 'Spades')
+    expect(@card.suit).to eq('Spades')    
+  end
+end
+=end
+
+
+# # EX. 3
+# class Card
+#   attr_reader :rank, :suit
+#   def initialize(rank, suit)
+#     @rank = rank
+#     @suit = suit
+#   end
+# end
+
+# RSpec.describe Card do
+#   def card                                                  #Helper method created
+#     Card.new('Ace', 'Spades')
+#   end
+#   it 'has a rank' do
+#     card = Card.new('Ace', 'Spades')                        #seperate arguments into their own indivisual test
+#     expect(card.rank).to eq('Ace')    
+#   end
+#   it 'has a suit' do
+#     card = Card.new('Ace', 'Spades')
+#     expect(card.suit).to eq('Spades')    
+#   end
+# end
+
+
+#   # EX. 3 (refactored)
+# class Card
+#   attr_accessor :rank, :suit                                   #Attr_accessor gives capability to read/write data
+#   def initialize(rank, suit)
+#     @rank = rank
+#     @suit = suit
+#   end
+# end
+
+# RSpec.describe Card do
+#   let(:card) {Card.new('Ace', 'Spades')}                    #'Let' RSpec method for cache data, each test will be reevaluated with a new card. 
+#                                                           # Allows argument to be referenced each time its called
+#                                                           # 'Let' only runs during the test being performed // only when needed
+
+#   it 'has a rank and that rank can change' do    
+#     expect(card.rank).to eq('Ace')    
+#     card.rank = "Queen"                                    #Tests that writer method is working as intended
+#     expect(card.rank).to eq('Queen')
+#   end
+#   it 'has a suit' do    
+#     expect(card.suit).to eq('Spades')    
+#   end
+# end
+
+# EX. 3 (personal test)
+# class Card
+#   attr_accessor :rank, :suit                                   #Attr_accessor gives capability to read/write data
+#   def initialize(rank, suit)
+#     @rank = rank
+#     @suit = suit
+#   end
+# end
+
+# RSpec.describe Card do
+#   let(:card) {Card.new('Ace', 'Spades')}                    #'Let' RSpec method for cache data, each test will be reevaluated with a new card. 
+#                                                           # Allows argument to be referenced each time its called
+#                                                           # 'Let' only runs during the test being performed
+#                                                           # 'let!' = forces the code to run similar to 'before' method
+
+#   it 'has a rank and that rank can change' do    
+#     expect(card.rank).to eq('Ace')    
+#     card.rank = "Queen"                                    #Tests that writer method is working as intended
+#     expect(card.rank).to eq('Queen')
+#   end
+#   it 'has a suit and that suit can change' do    
+#     expect(card.suit).to eq('Spades')    
+#     card.suit = 'Hearts'
+#     expect(card.suit).to eq('Hearts')
+#   end
+# end
+
+
+
+
+
