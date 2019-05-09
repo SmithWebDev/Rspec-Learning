1e8a9af8081ab3b459763f4832ada04329572ef3
Smith <smithwebdd@gmail.com>
2019-05-09 01:52:09 -0400

    Shared Examples

---
+++ spec/shared_examples_spec.rb
@@ -0,0 +1,57 @@
+#Shared example = An example that can be declared to be shared or utilized by multiple example groups
+
+#Following examples defy the DRY principle
+#####################################################
+# RSpec.describe Array do
+#   subject { [1,2,3] }
+
+#   it 'returns the number of items' do
+#     expect(subject.length).to eq(3)
+#   end
+# end
+
+# RSpec.describe String do
+#   subject { 'abc'}
+
+#   it 'returns the number of items' do
+#     expect(subject.length).to eq(3)
+#   end
+# end
+
+# RSpec.describe Hash do
+#   subject {{a:1, b:2, c:3}}
+# end
+######################################################
+
+
+RSpec.shared_examples 'a Ruby object with three elements' do
+  it 'returns the number of items' do
+    expect(subject.length).to eq(3)
+  end
+end
+
+RSpec.describe Array do
+  subject { [1,2,3] }
+  include_examples 'a Ruby object with three elements' 
+end
+
+RSpec.describe String do
+  subject { 'abc'}
+  include_examples 'a Ruby object with three elements' 
+end
+
+RSpec.describe Hash do
+  subject {{a:1, b:2, c:3}}
+  include_examples 'a Ruby object with three elements' 
+end
+
+class SausageLink
+  def length
+    3
+  end
+end
+
+RSpec.describe SausageLink do
+  subject { described_class.new}
+  include_examples 'a Ruby object with three elements' 
+end
